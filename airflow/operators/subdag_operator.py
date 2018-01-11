8c94156e62f53b7afe7a31317a30149452cb8ea5
Edgar Rodriguez <edgar.rodriguez@airbnb.com>
2018-01-11 01:17:50 -0800

    Reset subdag_operator

--- airflow/operators/subdag_operator.py
+++ airflow/operators/subdag_operator.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator, Pool
 from airflow.utils.decorators import apply_defaults
